6f3f6aa85ffa06d235d987b49127defe3971653e
Guilherme Cavichioli <guilherme.cavichioli@inspectral.com.br>
2025-05-30 00:40:07 -0300

    refactor(tests): reorganize store URL test structure and imports - Moved import statements for better readability - Reorganized test cases for storing URLs in the database

--- server/src/core/__tests__/store-url.spec.ts
+++ server/src/core/__tests__/store-url.spec.ts
@@ -1,50 +1,50 @@
-import { isError, isSuccess } from "@/shared/either";
 import { randomUUID } from "node:crypto";
-import { describe, expect, it } from "vitest";
-import { storeUrl } from "../store-url";
 import { db } from "@/infra/db";
 import { Schemas } from "@/infra/db/schemas";
+import { isError, isSuccess } from "@/shared/either";
 import { eq } from "drizzle-orm";
+import { describe, expect, it } from "vitest";
+import { storeUrl } from "../store-url";
 
-describe('store-url', () => {
-  it("should store an URL in database", async () => {
-    const randomName = `random-name-${randomUUID()}`;
+describe("store-url", () => {
+	it("should store an URL in database", async () => {
+		const randomName = `random-name-${randomUUID()}`;
 
-    const sut = await storeUrl({
-      url: "https://example.com",
-      shortUrl: randomName,
-    });
+		const sut = await storeUrl({
+			url: "https://example.com",
+			shortUrl: randomName,
+		});
 
-    expect(isSuccess(sut)).toBe(true)
+		expect(isSuccess(sut)).toBe(true);
 
-    const result = await db
-      .select()
-      .from(Schemas.url)
-      .where(eq(Schemas.url.shortUrl, randomName));
-    
-    expect(result).toHaveLength(1);
-  });
+		const result = await db
+			.select()
+			.from(Schemas.url)
+			.where(eq(Schemas.url.shortUrl, randomName));
 
-  it("should not store an URL in database if the shortUrl already exists", async () => {
-    const randomName = `random-name-${randomUUID()}`;
+		expect(result).toHaveLength(1);
+	});
 
-    await storeUrl({
-      url: "https://example.com",
-      shortUrl: randomName,
-    });
+	it("should not store an URL in database if the shortUrl already exists", async () => {
+		const randomName = `random-name-${randomUUID()}`;
 
-    const sut = await storeUrl({
-      url: "https://example.com",
-      shortUrl: randomName,
-    });
+		await storeUrl({
+			url: "https://example.com",
+			shortUrl: randomName,
+		});
 
-    expect(isError(sut)).toBe(true);
+		const sut = await storeUrl({
+			url: "https://example.com",
+			shortUrl: randomName,
+		});
 
-    const result = await db
-      .select()
-      .from(Schemas.url)
-      .where(eq(Schemas.url.shortUrl, randomName));
-    
-    expect(result).toHaveLength(1);
-  });
+		expect(isError(sut)).toBe(true);
+
+		const result = await db
+			.select()
+			.from(Schemas.url)
+			.where(eq(Schemas.url.shortUrl, randomName));
+
+		expect(result).toHaveLength(1);
+	});
 });

17e3ee7778bd3a3775c03c17a60eac57406f2051
Guilherme Cavichioli <guilherme.cavichioli@inspectral.com.br>
2025-05-30 00:40:18 -0300

    refactor(types): update URL validation and improve import structure
    - Changed import statements for clarity and consistency
    - Updated URL validation logic to use predefined schemas
    - Reformatted function parameters for better readability

--- server/src/core/store-url.ts
+++ server/src/core/store-url.ts
@@ -1,36 +1,37 @@
 import { db } from "@/infra/db";
 import { Schemas } from "@/infra/db/schemas";
-import { URLTableModel } from "@/infra/db/schemas/url";
+import type { URLTableModel } from "@/infra/db/schemas/url";
 import { URLAlreadyExistsError } from "@/infra/http/errors/url-already-exists";
-import { makeSuccess, makeError, Either } from "@/shared/either";
+import { type Either, makeError, makeSuccess } from "@/shared/either";
+import {
+	ShortUrlValidationSchema,
+	UrlValidationSchema,
+} from "@/shared/validations/url-schema";
 import { z } from "zod";
 
 const StoreUrlParams = z.object({
-  url: z.string().url(),
-  shortUrl: z.string().regex(
-    /^[a-zA-Z0-9\-._]+$/,
-    "Invalid characters in URL segment"
-  ),
+	url: UrlValidationSchema,
+	shortUrl: ShortUrlValidationSchema,
 });
 
 type Params = z.infer<typeof StoreUrlParams>;
 
-export async function storeUrl(input: Params): Promise<Either<URLAlreadyExistsError, URLTableModel>> {
-  const {
-    url,
-    shortUrl,
-  } = StoreUrlParams.parse(input);
+export async function storeUrl(
+	input: Params,
+): Promise<Either<URLAlreadyExistsError, URLTableModel>> {
+	const { url, shortUrl } = StoreUrlParams.parse(input);
 
-  try {
-    const response = await db
-      .insert(Schemas.url)
-      .values({
-        url,
-        shortUrl,
-      }).returning();
+	try {
+		const response = await db
+			.insert(Schemas.url)
+			.values({
+				url,
+				shortUrl,
+			})
+			.returning();
 
-    return makeSuccess(response[0]);
-  } catch (error) {
-    return makeError(new URLAlreadyExistsError());
-  }
+		return makeSuccess(response[0]);
+	} catch (error) {
+		return makeError(new URLAlreadyExistsError());
+	}
 }

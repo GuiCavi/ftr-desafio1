801dd74fb6b2731b97d8f1ad168e67f35d677a98
Guilherme Cavichioli <guilherme.cavichioli@inspectral.com.br>
2025-05-30 02:37:55 -0300

    refactor(server): improve code formatting and error handling
    - Reorganized import statements for better readability
    - Enhanced error handling in the setErrorHandler function
    - Improved formatting and indentation throughout the file

--- server/src/infra/http/server.ts
+++ server/src/infra/http/server.ts
@@ -5,9 +5,14 @@ import { fastifySwagger } from "@fastify/swagger";
 import { fastifySwaggerUi } from "@fastify/swagger-ui";
 import scalarUi from "@scalar/fastify-api-reference";
 import { fastify } from "fastify";
-import { hasZodFastifySchemaValidationErrors, jsonSchemaTransform, serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
-import { loadRoutes } from "./routes";
+import {
+	hasZodFastifySchemaValidationErrors,
+	jsonSchemaTransform,
+	serializerCompiler,
+	validatorCompiler,
+} from "fastify-type-provider-zod";
 import { ErrorCodes, ErrorKeyCodes } from "./errors/error-codes";
+import { loadRoutes } from "./routes";
 
 const server = fastify();
 
@@ -15,48 +20,49 @@ server.setValidatorCompiler(validatorCompiler);
 server.setSerializerCompiler(serializerCompiler);
 
 server.setErrorHandler((error, request, reply) => {
-  if (hasZodFastifySchemaValidationErrors(error)) {
-    return reply.status(400).send({
-      code: ErrorCodes.BAD_INPUT_FORMAT,
-      reason: ErrorKeyCodes.BAD_INPUT_FORMAT,
-      message: "Validation error",
-      issues: error.validation.flatMap(issue => ({
-        issue: issue.params.issue.code,
-        message: issue.params.issue.message,
-        param: issue.params.issue.path[0],
-      })),
-    })
-  }
+	if (hasZodFastifySchemaValidationErrors(error)) {
+		console.log("🚀 ~ server.setErrorHandler ~ error:", error);
+		return reply.status(400).send({
+			code: ErrorCodes.BAD_INPUT_FORMAT,
+			reason: ErrorKeyCodes.BAD_INPUT_FORMAT,
+			message: "Validation error",
+			issues: error.validation.flatMap((issue) => ({
+				issue: issue.params.issue.code,
+				message: issue.params.issue.message,
+				param: issue.params.issue.path[0],
+			})),
+		});
+	}
 
-  console.error(error);
+	console.error(error);
 
-  return reply.status(500).send({
-    code: ErrorCodes.INTERNAL_SERVER_ERROR,
-    reason: ErrorKeyCodes.INTERNAL_SERVER_ERROR,
-    message: "Internal server error",
-  });
+	return reply.status(500).send({
+		code: ErrorCodes.INTERNAL_SERVER_ERROR,
+		reason: ErrorKeyCodes.INTERNAL_SERVER_ERROR,
+		message: "Internal server error",
+	});
 });
 
 server.register(fastifyCors, { origin: "*" });
 
 server.register(fastifyMultipart);
 server.register(fastifySwagger, {
-  openapi: {
-    info: {
-      title: "brev.ly",
-      description: "URL shortener API",
-      version: "1.0.0",
-    }
-  },
-  transform: jsonSchemaTransform,
+	openapi: {
+		info: {
+			title: "brev.ly",
+			description: "URL shortener API",
+			version: "1.0.0",
+		},
+	},
+	transform: jsonSchemaTransform,
 });
 
 server.register(scalarUi, {
-  routePrefix: '/reference',
-})
+	routePrefix: "/reference",
+});
 
 server.register(fastifySwaggerUi, {
-  routePrefix: "/docs",
+	routePrefix: "/docs",
 });
 
 loadRoutes(server);
@@ -64,8 +70,6 @@ loadRoutes(server);
 const port = env.PORT || 3333;
 const host = "0.0.0.0";
 
-server
-  .listen({ port, host })
-  .then(() => {
-    console.log(`HTTP Server running on ${port}`);
-  });
+server.listen({ port, host }).then(() => {
+	console.log(`HTTP Server running on ${port}`);
+});

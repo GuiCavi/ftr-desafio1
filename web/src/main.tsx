8b0b8e0c9afcb5a8fee58086106162a2269fceb0
Guilherme Cavichioli <guilherme.cavichioli@inspectral.com.br>
2025-05-31 20:42:03 -0300

    feat(main): integrate Toaster and QueryClientProvider
    - Added Toaster component for notifications
    - Wrapped App component with QueryClientProvider for React Query

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,14 +1,21 @@
 import "./index.css";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Toaster } from "sonner";
 import { App } from "./app";
+
+const queryClient = new QueryClient();
 
 const rootElement = document.getElementById("root");
 if (rootElement) {
 	createRoot(rootElement).render(
 		<StrictMode>
-			<App />
+			<Toaster richColors />
+			<QueryClientProvider client={queryClient}>
+				<App />
+			</QueryClientProvider>
 		</StrictMode>,
 	);
 } else {

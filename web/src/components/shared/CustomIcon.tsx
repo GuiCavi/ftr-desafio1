e90db9749742e83e04e9aaeeb9467f5529df6d7f
Guilherme Cavichioli <guilherme.cavichioli@inspectral.com.br>
2025-05-31 16:37:44 -0300

    feat(icon): make className prop optional in CustomIcon component
    - Updated className prop in IconProps to be optional

--- web/src/components/shared/CustomIcon.tsx
+++ web/src/components/shared/CustomIcon.tsx
@@ -2,7 +2,7 @@ import { DynamicIcon } from "lucide-react/dynamic";
 import type { CSSProperties, ComponentProps } from "react";
 
 export type IconProps = ComponentProps<typeof DynamicIcon> & {
-	className: CSSProperties;
+	className?: CSSProperties;
 };
 
 export function CustomIcon({ name, size = 16, color, className }: IconProps) {

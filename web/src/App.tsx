12189bebd983dc9c6621cf3a60c581c3c58468c0
Guilherme Cavichioli <guilherme.cavichioli@inspectral.com.br>
2025-05-31 16:41:38 -0300

    feat(app): implement main App component layout
    - Add header with LogoIcon
    - Create form for original and shortened link inputs
    - Include buttons for saving links and downloading CSV
    - Structure layout with Card components

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,47 @@
-function App() {
-	return <h1>Hello</h1>;
-}
+import { Button } from "./components/Button";
+import { LogoIcon } from "./components/shared/LogoIcon";
+import Card from "./components/ui/Card";
+import { Input } from "./components/ui/Input";
 
-export default App;
+export function App() {
+	return (
+		<div className="w-full max-w-[980px] mx-auto px-3 pt-8">
+			<header className="flex items-center justify-center mb-6">
+				<LogoIcon />
+			</header>
+			<main className="flex flex-col gap-5 md:flex-row">
+				<Card.Container className="md:max-w-[380px]">
+					<Card.Header>Novo link</Card.Header>
+
+					<form action="" className="w-full flex flex-col gap-6">
+						<div className="flex flex-col gap-4">
+							<Input.Container>
+								<Input.Label htmlFor="url">Link original</Input.Label>
+								<Input type="url" name="url" placeholder="www.exemplo.com.br" />
+							</Input.Container>
+
+							<Input.Container>
+								<Input.Label htmlFor="url">Link encurtado</Input.Label>
+								<Input type="url" name="url" placeholder="brev.ly/" />
+							</Input.Container>
+						</div>
+
+						<Button disabled full>
+							Salvar link
+						</Button>
+					</form>
+				</Card.Container>
+
+				<Card.Container className="md:max-w-[580px]">
+					<Card.Header>
+						Meus links
+						<Button disabled variant="secondary" size="small">
+							<Button.Icon name="download" />
+							Baixar CSV
+						</Button>
+					</Card.Header>
+				</Card.Container>
+			</main>
+		</div>
+	);
+}
